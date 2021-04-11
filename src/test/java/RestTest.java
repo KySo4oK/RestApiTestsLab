9570ff57acac54036f13668226e62f1608cb4f35
KySo4oK <prostorostuk08@gmail.com>
2021-04-11 22:14:13 +0300

    add tests

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,2 +1,44 @@
+import models.FakeEntity;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class RestTest {
+
+    private FakeEntity fakeEntity;
+
+    @Test
+    public void testGetFakeEntity() {
+        when()
+                .get("http://localhost:7777/fake")
+                .then()
+                .statusCode(200)
+                .body(notNullValue());
+    }
+
+    @Test
+    public void testUpdateFakeEntity() {
+        when()
+                .put("http://localhost:7777/fake/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testCreateFakeEntity() {
+        fakeEntity = new FakeEntity(1L, "name");
+        when()
+                .post("http://localhost:7777/fake")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testDeleteFakeEntity() {
+        when()
+                .delete("http://localhost:7777/fake/1")
+                .then()
+                .statusCode(204);
+    }
 }
---
+++ src/test/java/models/FakeEntity.java
@@ -0,0 +1,7 @@
+package models;
+
+public class FakeEntity extends SimpleEntity {
+    public FakeEntity(long l, String name) {
+        super(l, name);
+    }
+}
